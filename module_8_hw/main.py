7b05ece0d929b8ce6481a6d7d9c96fe3239eaa03
Dmytro-Tarasenko <147235829+Dmytro-Tarasenko@users.noreply.github.com>
2023-11-07 11:20:34 +0200

    Add files via upload

--- module_8_hw/main.py
+++ module_8_hw/main.py
@@ -3,9 +3,6 @@ from datetime import date, datetime
 
 def get_birthdays_per_week(users):
 
-    if len(users) == 0:
-        return {}
-
     users_bd = {
         'Monday': [],
         'Tuesday': [],
@@ -13,26 +10,32 @@ def get_birthdays_per_week(users):
         'Thursday': [],
         'Friday': []
     }
-    # Реалізуйте тут домашнє завдання
+
     for user in users:
-        bd_to_be = datetime(date.today().year, user['birthday'].month, user['birthday'].day).date()
+        bd_to_be = datetime(date.today().year,
+                            user['birthday'].month,
+                            user['birthday'].day).date()
         cur_date = date.today()
         delta = bd_to_be - cur_date
 
         if 0 <= delta.days <= 6:
             day_of_week = datetime.strftime(bd_to_be, '%A')
-            day_of_week = day_of_week if day_of_week in users_bd.keys() else 'Monday'
+            day_of_week = day_of_week \
+                if day_of_week in users_bd.keys() else 'Monday'
             users_bd[day_of_week].append(user['name'].split(' ')[0])
-
-        if -365 <= delta.days < -358:
-            bd_to_be = datetime(date.today().year+1, user['birthday'].month, user['birthday'].day).date()
+        elif delta.days < -358:
+            bd_to_be = datetime(date.today().year+1,
+                                user['birthday'].month,
+                                user['birthday'].day).date()
             delta = bd_to_be - cur_date
             if 0 <= delta.days <= 6:
                 day_of_week = datetime.strftime(bd_to_be, '%A')
-                day_of_week = day_of_week if day_of_week in users_bd.keys() else 'Monday'
+                day_of_week = day_of_week \
+                    if day_of_week in users_bd.keys() else 'Monday'
                 users_bd[day_of_week].append(user['name'].split(' ')[0])
 
-    users = {day: user_list for day, user_list in users_bd.items() if len(user_list) > 0}
+    users = {day: user_list for day, user_list in users_bd.items()
+             if len(user_list) > 0}
 
     return users
 
